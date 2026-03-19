6f16400218da4abe0944948610ba7401722a220b
pawanbhatt6227 <pawanbhatt6227@gmail.com>
2026-03-19 13:54:28 +0530

    episode-01

---
+++ app.js
@@ -0,0 +1,20 @@
+// html structure
+
+// <div id="parent"></div>
+//     <div id="child">
+//         <h1>here is h1</h1>
+//         <h2>here is h2</h2>
+//     </div>
+// </div>
+
+//nesting and siblings
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "here is h1"),
+    React.createElement("h2", {}, "here is h2"),
+  ]),
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);

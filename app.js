2f131ed9b1c9a73f2aadf69586946438712fb7e8
pawanbhatt6227 <pawanbhatt6227@gmail.com>
2026-03-21 12:30:32 +0530

    jsx intro

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// html structure
 
-// <div id="parent"></div>
-//     <div id="child">
-//         <h1>here is h1</h1>
-//         <h2>here is h2</h2>
-//     </div>
-// </div>
+//jsx -- converted to react element by BABEL ...
 
-//nesting and siblings
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { key: "h1" }, "here is h1"),
-    React.createElement("h2", { key: "h2" }, "here is h2"),
-  ]),
+const jsxHeading = (
+  <h2 id="newHeading" className="newclass" tabIndex="1">
+    "Hello World from jsx"
+  </h2>
+);
+const heading = React.createElement("h1", { id: "hello" }, "hello World");
+
+const final = (
+  <div>
+    {jsxHeading}
+    {heading}
+  </div>
 );
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(final);
